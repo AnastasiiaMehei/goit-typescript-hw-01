9cda51c32e0926dedd76593aeb2ecdcf5dcba21c
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-06-19 17:42:30 +0300

    add

--- vite-project/src/generics/2.ts
+++ vite-project/src/generics/2.ts
@@ -1,15 +1,18 @@
-// type AllType = {
-//   name: string;
-//   position: number;
-//   color: string;
-//   weight: number;
-// };
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
 
-// function compare(top, bottom): AllType {
-//   return {
-//     name: top.name,
-//     color: top.color,
-//     position: bottom.position,
-//     weight: bottom.weight,
-//   };
-// }
+function compare(
+  top: AllType,
+  bottom: AllType
+): Pick<AllType, "name" | "color" | "position" | "weight"> {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
